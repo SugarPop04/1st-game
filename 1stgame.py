31f19e483758570b9e44bbf2ea21441ed1af66a6
Sweetfizz <158744436+SugarPop04@users.noreply.github.com>
2024-09-16 18:44:57 +0530

    Update 1stgame.py
    getting used to the language

--- 1stgame.py
+++ 1stgame.py
@@ -3,62 +3,61 @@ from time import sleep
 
 def rock(b, a):
     if a == b:
-        print(f"It's a tie!, The computer chooses also chooses {a}")
+        print(f"It's a tie! The computer also chooses {a}.")
     elif a == "paper":
-        print(f"You lose! The computer choose {a}.")
+        print(f"You lose! The computer chooses {a}.")
     elif a == "scissors":
-        print(f"You win! The computer choose {a}.")
+        print(f"You win! The computer chooses {a}.")
 
 def paper(b, a):
     if a == b:
-        print(f"It's a tie!, The computer chooses also chooses {a}")
+        print(f"It's a tie! The computer also chooses {a}.")
     elif a == "scissors":
-        print(f"You lose! The computer choose {a}.")
+        print(f"You lose! The computer chooses {a}.")
     elif a == "rock":
-        print(f"You win! The computer choose {a}.")
+        print(f"You win! The computer chooses {a}.")
 
 def scissors(b, a):
     if a == b:
-        print(f"It's a tie!, The computer chooses also chooses {a}")
+        print(f"It's a tie! The computer also chooses {a}.")
     elif a == "rock":
-        print(f"You lose! The computer choose {a}.")
+        print(f"You lose! The computer chooses {a}.")
     elif a == "paper":
-        print(f"You win! The computer choose {a}.")
+        print(f"You win! The computer chooses {a}.")
 
 def computer():
     return random.choice(["rock", "paper", "scissors"])
 
 def play():
     outputs = ["rock", "paper", "scissors"]
-    a= computer()
-    b = input("Welcome! Enter rock, paper, or scissors: ").lower()
-
     while True:
+        b = input("Welcome! Enter rock, paper, or scissors: ").lower()
         if b not in outputs:
-            print("Invalid input. Please enter again")
+            print("Invalid input. Please enter again.")
             sleep(1)
-            
-        match b:
-            case "rock":
-                rock(b, a)
-            case "paper":
-                paper(b, a)
-            case "scissors":
-                scissors(b, a)
-        break   
+        else:
+            a = computer()
+            match b:
+                case "rock":
+                    rock(b, a)
+                case "paper":
+                    paper(b, a)
+                case "scissors":
+                    scissors(b, a)
+            break
 
 def play_again():
     while True:
-        answer = input("Would you like to play again? ").lower()
+        answer = input("Would you like to play again? (yes/no) ").lower()
         if answer.startswith("y"):
             return True
-        if answer.startswith("n"):
+        elif answer.startswith("n"):
             return False
         else:
             print("Please enter 'yes' or 'no'.")
             sleep(1)
 
-play() 
+play()
 
 while True:
     if play_again():
